599ecd9320780627346b850bf77613697308abbf
Jogi <stigmahotspot@gmail.com>
2022-11-04 00:10:37 +0700

    push3

--- script.js
+++ script.js
@@ -1,73 +1,145 @@
-// ternary operator untuk test local
-const testStorage = typeof(Storage) !== "undefined" ? "local storage available" : "local storage not available";
 
-console.log(testStorage)
+allData()
 
 
-document.getElementById('push').onclick = function(){
-  if(document.getElementById('isitask').value.length == 0 || document.getElementById('judul').value.length == 0 || document.getElementById('tanggal').value.length === 0){
-      alert("Tidak boleh kosong")
-  }
-
-  else{
-    let judul = document.getElementById('judul').value;
-    let tanggal = document.getElementById('tanggal').value;
-    let isitask = document.getElementById('isitask').value;
-    let data = {
-        judul:judul,
-        tanggal:tanggal,
-        isitask:isitask
-    };
-
-//       document.getElementById('tasks').innerHTML += `
-//       <div class="task rounded-4 mb-3">
-//           <span id="taskname" >`
-//           Object.values(data).forEach((page) => 
-//               (page)
-//           )
-//           `</span>
-//           <button class="delete">
-//               <i class="far fa-trash-alt"></i>
-//           </button>
-//       </div>
-//   `;
-      
-
-
-      Object.values(data).forEach((page) =>  console.log(page));
-      
-      document.getElementById('tasks').innerHTML += `
-          <div class="task rounded-4 mb-3">
-              <span id="taskname" >
-                  ${data.judul} <br>
-                  ${data.tanggal} <br>
-                  ${data.isitask} <br>
-              </span>
-              <button class="delete">
-              <i class="far fa-trash-alt"></i>
-            </button>
-          </div>
-      `
-      let arr = [];
-      if(!localStorage.getItem('data')) {
-          arr.push(data);
-          localStorage.setItem("data", JSON.stringify(arr));
-        } else {
-            arr = JSON.parse(localStorage.getItem("data"));
-            arr.push(data);
-            localStorage.setItem("data", JSON.stringify(arr));
-        }
-
-      var current_tasks = document.getElementsByClassName("delete");
-      for(var i=0; i<current_tasks.length; i++){
-          current_tasks[i].onclick = function(){
-              this.parentNode.remove();
-              localStorage.removeItem('data')
-          }
-      }
-  }
-  judul.value = ""
-  tanggal.value = ""
-  isitask.value =""
+function clearData(){
+    document.getElementById('form').reset()
+    document.getElementById('id').value = ""
 }
 
+
+    
+
+
+//method to get all data
+function allData(){
+    
+    table.innerHTML = ``
+    //get data from localstorage and store to contaclist array
+    //we must to use JSON.parse, because data as string, we need convert to array
+    contactList = JSON.parse(localStorage.getItem('listItem')) ?? []
+
+    //looping data and show data in table
+    contactList.forEach(function (value, i){
+       
+        var table = document.getElementById('table')
+
+        table.innerHTML += `
+            <tr class>
+                <td><strong>Judul</strong></td>
+            </tr>
+            <tr>
+                <td>${value.judul}</td>
+            </tr>
+            <tr>
+                <td><strong>Tanggal</strong></td>
+            </tr>
+            <tr>
+                <td>${value.phone}</td>
+            </tr>
+            <tr>
+                <td><strong>List</strong></td>
+            </tr>
+            <tr>
+                <td>${value.address}</td>
+            </tr>
+            <tr class="mb-3">
+                <td>
+                    <button class="btn btn-sm btn-success" onclick="find(${value.id})">
+                        <i class="fa fa-edit"></i>
+                    </button>
+                    <button class="btn btn-sm btn-danger" onclick="removeData(${value.id})">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </td>
+            </tr>`
+    })
+}
+
+
+    
+
+//method to save data into localstorage
+function save(){
+//get data from localstorage and store to contaclist array
+//we must to use JSON.parse, because data as string, we need convert to array
+contactList = JSON.parse(localStorage.getItem('listItem')) ?? []
+
+//get last array to get last id
+//and store it into variable id
+var id
+contactList.length != 0 ? contactList.findLast((item) => id = item.id) : id = 0
+
+if(document.getElementById('id').value){
+
+//edit contactlist array based on value
+contactList.forEach(value => {
+    if(document.getElementById('id').value == value.id){
+        value.judul      = document.getElementById('juduk').value, 
+        value.phone     = document.getElementById('phone').value,
+        value.address   = document.getElementById('address').value
+    }
+});
+
+//remove hidden input
+document.getElementById('id').value = ''
+
+}else{
+
+//save
+//get data from form
+var item = {
+    id        : id + 1, 
+    judul      : document.getElementById('judul').value,  
+    phone     : document.getElementById('phone').value,
+    address   : document.getElementById('address').value
+}
+
+//add item data to array contactlist
+contactList.push(item)
+}
+
+// save array into localstorage
+localStorage.setItem('listItem', JSON.stringify(contactList))
+
+//update table list
+allData()
+
+//remove form data
+document.getElementById('form').reset()
+}
+
+
+//method to get detail personal data based on id
+function find(id){
+//get data from localstorage and store to contaclist array
+//we must to use JSON.parse, because data as string, we need convert to array
+contactList = JSON.parse(localStorage.getItem('listItem')) ?? []
+
+contactList.forEach(function (value){
+if(value.id == id){
+   document.getElementById('id').value = value.id
+   document.getElementById('judul').value = value.judul
+   document.getElementById('phone').value = value.phone
+   document.getElementById('address').value = value.address
+}
+})
+}
+
+
+
+function removeData(id){
+//get data from localstorage and store to contaclist array
+//we must to use JSON.parse, because data as string, we need convert to array
+contactList = JSON.parse(localStorage.getItem('listItem')) ?? []
+
+contactList = contactList.filter(function(value){ 
+    return value.id != id; 
+});
+
+// save array into localstorage
+localStorage.setItem('listItem', JSON.stringify(contactList))
+
+//get data again
+allData()
+}
